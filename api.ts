b0312c2fdf8c1b79bf0f62adae3f8e68361d9920
nemusona <nemusonaneko@protonmail.com>
2023-02-19 03:52:41 -0600

    some fixes

--- api.ts
+++ api.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import * as dotenv from "dotenv";
 dotenv.config();
 import * as bodyParser from "body-parser";
@@ -51,73 +51,77 @@ const queueEvents = new QueueEvents("nemu", {
 });
 
 /// Get status of API
-app.get("/", (res: Response) => {
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
   return res.status(200).send("API is Alive");
 });
 
 /// Get queue from bull
-app.get("/queue", async (res: Response) => {
+app.get("/queue", async (req: Request, res: Response, next: NextFunction) => {
   const result = await queue.getJobCounts();
   const count =
     Number(result.active) + Number(result.delayed) + Number(result.waiting);
-  return res.status(200).send(count);
+  return res.status(200).send(count.toString());
 });
 
 /// Send request to queue
-app.post("/generate", jsonParser, async (req: Request, res: Response) => {
-  try {
-    /// Get current queue
-    const queueResult = await queue.getJobCounts();
-    const count =
-      Number(queueResult.active) +
-      Number(queueResult.delayed) +
-      Number(queueResult.waiting);
+app.post(
+  "/generate",
+  jsonParser,
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      /// Get current queue
+      const queueResult = await queue.getJobCounts();
+      const count =
+        Number(queueResult.active) +
+        Number(queueResult.delayed) +
+        Number(queueResult.waiting);
 
-    /// Revert with 503 code if limit already reached
-    if (count > Number(queueLimit)) {
-      return res.status(503).send("Cannot process request");
+      /// Revert with 503 code if limit already reached
+      if (count > Number(queueLimit)) {
+        return res.status(503).send("Cannot process request");
+      }
+
+      /// Load in default payload
+      const payload = { ...defaultPayload };
+
+      /// Load in settings from request
+      const positive = req.body.prompt;
+      const negative_prompt = req.body.negative_prompt;
+      const cfg_scale = req.body.cfg_scale;
+      const denoising_strength = req.body.denoising_strength;
+      if (positive) {
+        payload.prompt += `, ${positive.toString()}`;
+      }
+      if (negative_prompt) {
+        payload.negative_prompt += `, ${negative_prompt.toString()}`;
+      }
+      if (cfg_scale) {
+        payload.cfg_scale = cfg_scale;
+      }
+      if (denoising_strength) {
+        payload.denoising_strength = denoising_strength;
+      }
+
+      /// Add to queue with delay
+      const job = await queue.add("prompt", payload, {
+        removeOnComplete: true,
+        removeOnFail: true,
+        delay: Number(queueDelay),
+      });
+
+      /// Get base64 response from worker
+      const base64 = await job.waitUntilFinished(
+        queueEvents,
+        Number(queueTimeout)
+      );
+
+      /// Return base64 to client
+      return res.status(200).send(base64.toString());
+    } catch (error) {
+      return res.status(500).send("Server Error");
     }
-
-    /// Load in default payload
-    const payload = { ...defaultPayload };
-
-    /// Load in settings from request
-    const positive = req.body.prompt;
-    const negative_prompt = req.body.negative_prompt;
-    const cfg_scale = req.body.cfg_scale;
-    const denoising_strength = req.body.denoising_strength;
-    if (positive) {
-      payload.prompt += `, ${positive.toString()}`;
-    }
-    if (negative_prompt) {
-      payload.negative_prompt += `, ${negative_prompt.toString()}`;
-    }
-    if (cfg_scale) {
-      payload.cfg_scale = cfg_scale;
-    }
-    if (denoising_strength) {
-      payload.denoising_strength = denoising_strength;
-    }
-
-    /// Add to queue with delay
-    const job = await queue.add("prompt", payload, {
-      removeOnComplete: true,
-      removeOnFail: true,
-      delay: Number(queueDelay),
-    });
-
-    /// Get base64 response from worker
-    const base64 = await job.waitUntilFinished(
-      queueEvents,
-      Number(queueTimeout)
-    );
-
-    /// Return base64 to client
-    return res.status(200).send(base64);
-  } catch (error) {
-    return res.status(500).send("Server Error");
   }
-});
+);
 
 app.listen(port, () => {
   console.log(`Running on port ${port}`);
